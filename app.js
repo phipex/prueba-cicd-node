deb33873e3d105880c68ba71ac109d3073e78d9d
phipex <aphipex@gmail.com>
2023-03-08 15:13:04 -0500

    cambios en el mensaje

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // Hello World sample app.
 const http = require('http');
+import { platform } from 'node:process';
+import { versions } from 'node:process';
 
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello World. This processor architecture is ${process.arch}`);
+  res.end(`Hello World. This processor architecture is ${process.arch} platform ${platform} versions ${versions}`);
 });
 
 server.listen(port, () => {
-  console.log(`Server running on processor architecture ${process.arch}`);
+  console.log(`Server running on processor architecture ${process.arch} puerto ${port}`);
 });
